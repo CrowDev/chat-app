49af6ba4d1709b59887283ed7acb336f9a5497ec
Hardy Aguilar <h.aguilarvalenzuela@gmail.com>
2025-07-28 16:32:06 -0400

    refactor: handle logic for ai response

--- src/hooks/useMessages.ts
+++ src/hooks/useMessages.ts
@@ -3,13 +3,19 @@ import { useToken } from "./useToken";
 import { mockApi } from "@/api/mockApi";
 import type { Conversation, Message } from "@/types";
 import { useConversationsContext } from "./useConversationsContext";
+import { getMessages, storeMessages } from "@/storage/localStorage";
 
-export const useMessages = (conversationId: string | undefined) => {
+export const useMessages = (conversationId: string) => {
   const { token } = useToken(Number(conversationId));
   const { conversations } = useConversationsContext();
   const [messages, setMessages] = useState<Message[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<boolean>(false);
+  const [isTyping, setIsTyping] = useState<boolean>(false);
+
+  const checkMessages = () => {
+    return getMessages(conversationId);
+  };
 
   useEffect(() => {
     const fetchConversation = async () => {
@@ -18,44 +24,71 @@ export const useMessages = (conversationId: string | undefined) => {
         const result = await mockApi.getMessages(token, conversationId);
 
         if (result.messages.length > 0) {
+          storeMessages(conversationId, result.messages);
           setMessages(result.messages);
         } else {
-          // if there's no messages then is a new conversation.
-          // We look for the new created conversation and we use
-          // the title as input for the message
-          const currentConversation = conversations.find(
-            (conversation: Conversation) =>
-              conversation.id === Number(conversationId),
-          );
-          if (currentConversation) {
-            const { title } = currentConversation;
-            sendMessage(title);
-          }
+          getNewConversationMessage();
         }
+        setLoading(false);
       } catch (error) {
         console.error(error);
         setError(true);
-      } finally {
         setLoading(false);
       }
     };
 
-    fetchConversation();
+    const messagesStore = checkMessages();
+    if (messagesStore) {
+      setMessages(messagesStore);
+    } else {
+      fetchConversation();
+    }
   }, [token, conversationId]);
 
+  const getNewConversationMessage = () => {
+    // if there's no messages then is a new conversation.
+    // We look for the new created conversation and we use
+    // the title as input for the message
+    const currentConversation = conversations.find(
+      (conversation: Conversation) =>
+        conversation.id === Number(conversationId),
+    );
+    if (currentConversation) {
+      const { title } = currentConversation;
+      sendMessage(title);
+    }
+  };
+
   const sendMessage = async (message: string) => {
-    const result = await mockApi.sendMessage(token, conversationId, message);
-    const aiResult = await mockApi.simulateAIResponse(conversationId, message);
-    setMessages((prev: Message[]) => [
-      ...prev,
-      result.message,
-      aiResult.message,
-    ]);
+    setError(false);
+    try {
+      const userMsg = await mockApi.sendMessage(token, conversationId, message);
+      setMessages((prev) => [...prev, userMsg.message]);
+
+      setIsTyping(true);
+
+      const aiResponse = await mockApi.simulateAIResponse(
+        conversationId,
+        message,
+      );
+      setIsTyping(false);
+      setMessages((prev) => [...prev, aiResponse.message]);
+      storeMessages(conversationId, [
+        ...messages,
+        userMsg.message,
+        aiResponse.message,
+      ]);
+    } catch (error) {
+      console.error("Failed to send message:", error);
+      setIsTyping(false);
+      setError(true);
+    }
   };
 
   return {
     messages,
     sendMessage,
+    isTyping,
     error,
     loading,
   };
