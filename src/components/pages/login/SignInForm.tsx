7465fb47e4e4311863cc9ac060adeff5b24b0aac
Hardy Aguilar <h.aguilarvalenzuela@gmail.com>
2025-07-27 20:14:26 -0400

    feat: add auth form

--- src/components/pages/login/SignInForm.tsx
+++ src/components/pages/login/SignInForm.tsx
@@ -1,30 +1,71 @@
+import { useForm, type SubmitHandler } from "react-hook-form";
+import { mockApi } from "@/api/mockApi";
+
+interface ISignInForm {
+  email: string;
+  password: string;
+  form: string;
+}
+
 export const SignInForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+    clearErrors,
+    setValue,
+  } = useForm<ISignInForm>();
+  const onSubmit: SubmitHandler<ISignInForm> = async (data) => {
+    try {
+      const result = await mockApi.login(data.email, data.password);
+      localStorage.setItem("token", result.token);
+    } catch (error) {
+      setValue("password", "");
+      setError("form", {
+        type: "custom",
+        message: "Invalid credentials",
+      });
+    }
+  };
+
+  const clearError = () => clearErrors("form");
+
   return (
     <div>
-      <form className="space-y-4">
+      <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
         <div className="flex flex-col space-y-2">
           <label htmlFor="email">Email</label>
           <input
             id="email"
-            name="email"
             type="email"
             placeholder="Enter your email"
             className="py-2 px-3 rounded-lg border-slate-600 border focus:outline-none focus-visible:ring focus-visible:ring-offset-2 focus-visible:ring-primary focus-visible:ring-offset-primary"
+            {...register("email", { required: true })}
+            onChange={clearError}
           />
+          {errors.email && (
+            <span className="text-red-500">This field is required</span>
+          )}
         </div>
         <div className="flex flex-col space-y-2">
           <label htmlFor="password">Password</label>
           <input
             id="password"
-            name="password"
             type="password"
             placeholder="Enter your password"
             className="py-2 px-3 rounded-lg border-slate-600 border focus:outline-none focus-visible:ring focus-visible:ring-offset-2 focus-visible:ring-primary focus-visible:ring-offset-primary"
+            {...register("password", { required: true })}
+            onChange={clearError}
           />
+          {errors.password && (
+            <span className="text-red-500">This field is required</span>
+          )}
         </div>
+        {errors.form && <p className="text-red-500">Invalid credentials</p>}
         <div>
           <button
-            type="button"
+            type="submit"
             className="rounded-lg hover:cursor-pointer bg-primary hover:bg-primary/90 disabled:bg-primary/50 disabled:hover:cursor-default w-full py-2 px-4"
           >
             Sign In
