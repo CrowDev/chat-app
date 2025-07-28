7eb97f00737c13368fb18084212fc7a55894b7fb
Hardy Aguilar <h.aguilarvalenzuela@gmail.com>
2025-07-28 13:37:08 -0400

    feat: add error handler for sidebar

--- src/components/layout/sidebar/Sidebar.tsx
+++ src/components/layout/sidebar/Sidebar.tsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router";
-import { CirclePlus } from "lucide-react";
+import { CirclePlus, RotateCcw, TriangleAlert } from "lucide-react";
 import { useConversationsContext } from "@/hooks/useConversationsContext";
 import { LoadingSidebar } from "@/components/ui/loading/LoadingSidebar";
 import { SidebarListConversation } from "@/components/ui/sidebar/SidebarListConversation";
 
 export const Sidebar = () => {
-  const { conversations, loading } = useConversationsContext();
+  const { conversations, loading, refetch, error } = useConversationsContext();
 
   return (
     <div className="min-h-dvh border-r-slate-600 border-r p-4 flex flex-col space-y-4">
@@ -17,6 +17,21 @@ export const Sidebar = () => {
           </Link>
         </div>
         <div className="text-xs">Chats</div>
+        {error && !loading && (
+          <div className="flex gap-2">
+            <span className="text-xs text-red-500">
+              Failed to fetch conversations
+              <TriangleAlert size={12} />
+            </span>
+            <button
+              type="button"
+              className="focus:outline-none hover:cursor-pointer hover:-rotate-360 hover:scale-110 transition-transform duration-500"
+              onClick={refetch}
+            >
+              <RotateCcw size={16} />
+            </button>
+          </div>
+        )}
         {loading ? (
           <LoadingSidebar />
         ) : (
