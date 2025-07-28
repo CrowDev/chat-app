3f1e94dbe1e32e596d4abe212ba15efdfb9ee3fe
Hardy Aguilar <h.aguilarvalenzuela@gmail.com>
2025-07-27 22:18:31 -0400

    feat: add basic sidebar

--- src/components/layout/sidebar/Sidebar.tsx
+++ src/components/layout/sidebar/Sidebar.tsx
@@ -1,3 +1,47 @@
+import { useEffect, useState } from "react";
+import { mockApi } from "@/api/mockApi";
+import type { Conversation } from "@/types";
+import { Link } from "react-router";
+
+const TOKEN_KEY = "token";
+
 export const Sidebar = () => {
-  return <div className="min-h-dvh bg-blue-500">Sidebar</div>;
+  const [conversations, setConversations] = useState<Conversation[]>([]);
+
+  useEffect(() => {
+    const fetchConversations = async () => {
+      const token = localStorage.getItem(TOKEN_KEY);
+      if (token) {
+        try {
+          const result = await mockApi.getConversations(token);
+          setConversations(result.conversations);
+        } catch (error) {
+          console.log("Failed to load conversations");
+        }
+      }
+    };
+    fetchConversations();
+  }, []);
+
+  return (
+    <div className="min-h-dvh border-r-slate-600 border-r p-4 flex flex-col space-y-4">
+      <div>logo</div>
+      <div className="flex flex-col space-y-4 mt-2">
+        <div className="text-xs">Chats</div>
+        <ul className="flex flex-col space-y-1">
+          {conversations.map((conversation: Conversation) => {
+            return (
+              <li className="overflow-hidden rounded-lg hover:bg-slate-600 hover:cursor-pointer transition-colors p-1.5">
+                <p className="truncate text-sm">
+                  <Link to={`/chat/${conversation.id}`}>
+                    {conversation.title}
+                  </Link>
+                </p>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 };
