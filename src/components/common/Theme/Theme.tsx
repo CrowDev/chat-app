f6af30796a8ed95c8eb23d244a0851eb4453aacb
Hardy Aguilar <h.aguilarvalenzuela@gmail.com>
2025-07-29 02:26:32 -0400

    feat: handle theme storage

--- src/components/common/Theme/Theme.tsx
+++ src/components/common/Theme/Theme.tsx
@@ -1,9 +1,33 @@
 import { SunMoon } from "lucide-react";
+import { useEffect } from "react";
+
+const STORE_KEY = "theme";
+
 export const Theme = () => {
+  const htmlElement = document.documentElement;
+
+  useEffect(() => {
+    const handleThemeStore = () => {
+      const theme = localStorage.getItem(STORE_KEY);
+      if (!theme) {
+        localStorage.setItem(STORE_KEY, "dark");
+        htmlElement.classList.add("dark");
+      } else {
+        htmlElement.classList.add(theme);
+      }
+    };
+    handleThemeStore();
+  }, []);
+
   const handleChangeTheme = () => {
-    const htmlElement = document.documentElement;
+    if (htmlElement.classList.contains("dark")) {
+      localStorage.setItem("theme", "light");
+    } else {
+      localStorage.setItem("theme", "dark");
+    }
     htmlElement.classList.toggle("dark");
   };
+
   return (
     <div className="z-50 absolute top-5 right-5 text-light-primary-text dark:text-dark-primary-text">
       <button
