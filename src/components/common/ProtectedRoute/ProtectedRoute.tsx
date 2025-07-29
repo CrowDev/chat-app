9810b9096ebe6bc2eb6ad17ef08534a1be15637e
Hardy Aguilar <h.aguilarvalenzuela@gmail.com>
2025-07-29 02:07:10 -0400

    feat: improve loader for protected routes

--- src/components/common/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/common/ProtectedRoute/ProtectedRoute.tsx
@@ -1,12 +1,18 @@
 import { useAuth } from "@/hooks/useAuth";
 import { Outlet } from "react-router";
 import { Navigate } from "react-router";
+import { Spinner } from "../Spinner/Spinner";
 
 export const ProtectedRoute = () => {
   const { isAuthenticated, loading } = useAuth();
 
-  // TODO: create a good loading ui
-  if (loading) return <div>Loading...</div>;
+  if (loading) {
+    return (
+      <div className="min-h-dvh grid place-content-center bg-light-main-bg dark:bg-dark-main-bg text-light-primary-text dark:text-dark-primary-text">
+        <Spinner size={48} />
+      </div>
+    );
+  }
 
   if (!isAuthenticated) {
     return <Navigate to="/login" replace />;
